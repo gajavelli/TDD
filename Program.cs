f78780c8fff5335865222003aa2bf168268b5a41
Prashant Gajavelli <gajavelli@gmail.com>
2019-07-18 17:17:41 -0400

    Made code more readable

--- Program.cs
+++ Program.cs
@@ -1,43 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-//using NUnit.Framework;
 using Xunit;
 
 namespace TDD
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            List<Item> mainItems = new List<Item>();
-            List<Item> randomItems = new List<Item>();
-            string TestletId = "TestletId1";
-            //The items added to the mainItems are not in any particular order
-            //...you could mix match in any order
-            //however the final randomized list that 
+        static void Main(string[] args) {
+            List<Item> testQuestions = new List<Item>();
+            List<Item> randomizedQuestions = new List<Item>();
+            string assessmentId = "AssessmentId1";
+            //The items added to the testQuestions list are not in any particular order
+            //...you could mix match and add in any order
+            //however the final randomized list should have the first and second 
+            //question always of type pretest and rest eight mixed
 
-            mainItems.Add( new Item("operational_item1", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("operational_item2", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("operational_item3", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("pretest_item1", ItemTypeEnum.Pretest));
-            mainItems.Add( new Item("pretest_item2", ItemTypeEnum.Pretest));
-            mainItems.Add( new Item("operational_item4", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("operational_item5", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("operational_item6", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("pretest_item3", ItemTypeEnum.Pretest));
-            mainItems.Add( new Item("pretest_item4", ItemTypeEnum.Pretest));
+            testQuestions.Add( new Item("operational_item1", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("operational_item2", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("operational_item3", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("pretest_item1", ItemTypeEnum.Pretest));
+            testQuestions.Add( new Item("pretest_item2", ItemTypeEnum.Pretest));
+            testQuestions.Add( new Item("operational_item4", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("operational_item5", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("operational_item6", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("pretest_item3", ItemTypeEnum.Pretest));
+            testQuestions.Add( new Item("pretest_item4", ItemTypeEnum.Pretest));
             
-            
-            randomItems =  CreateTestAndRandomize(TestletId, mainItems);
+            randomizedQuestions =  CreateTestAndRandomize(assessmentId, testQuestions);
             //Print to Console
-            randomItems.ForEach(item => Console.WriteLine(item.ItemId));
-
+            randomizedQuestions.ForEach(item => Console.WriteLine(item.ItemId));
         }
-        public static List<Item> CreateTestAndRandomize(string testletId, List<Item> mainItems)
-        {
-            Testlet testlet = new Testlet(testletId, mainItems);
-             return testlet.Randomize();
+        public static List<Item> CreateTestAndRandomize(string testletId, List<Item> testQuestions) {
+            Testlet testlet = new Testlet(testletId, testQuestions);
             
+            return testlet.Randomize();
         }
 
     }
@@ -46,41 +42,40 @@ namespace TDD
          public string TestletId;     
          private List<Item> Items; 
         static Random _random = new Random();
-        public Testlet(string testletId, List<Item> items)     
-        {         
+        public Testlet(string testletId, List<Item> items) {         
             TestletId = testletId;         
             Items = items;     
         } 
         
-        public List<Item> Randomize()     {         //Items private collection has 6 Operational and 4 Pretest Items. Randomize the order of these items as per the requirement (with TDD) 
+        public List<Item> Randomize() {         
+            //Items private collection has 6 Operational and 4 Pretest Items. 
+            //...Randomize the order of these items as per the requirement (with TDD) 
+            
             List<Item> returnItems = new List<Item>();
-            int n  =  Items.Count;
-            Item[] array = Items.ToArray();
+            int totalCount  =  Items.Count;
+            Item[] itemsArray = Items.ToArray();
             List<Item> tempItems = new List<Item>();
 
-            for (int i = 0; i < n; i++)
-            {
+            for (int index = 0; index < totalCount; index++) {
                 // Use Next on random instance with an argument.
                 // ... The argument is an exclusive bound.
                 //     So we will not go past the end of the array.
-                int r = i + _random.Next(n - i);
-                //Console.WriteLine(r);
-                Item t = array[r];
-                if(returnItems.Count < 2 && t.ItemType == 0)
-                {
-                   returnItems.Add(t);
+                int randomIndex = index + _random.Next(totalCount - index);
+                Item randomItem = itemsArray[randomIndex];
+                
+                if(returnItems.Count < 2 && randomItem.ItemType == 0) {
+                   returnItems.Add(randomItem);
                 }
-                else{
-                    tempItems.Add(t);
+                else {
+                    tempItems.Add(randomItem);
                 }
-                array[r] = array[i];
-                array[i] = t;
+                itemsArray[randomIndex] = itemsArray[index];
+                itemsArray[index] = randomItem;
             }
 
             //Add the tempItems to the bottom of the returnItems
             returnItems.AddRange(tempItems);
             //returnItems.ForEach(item => Console.WriteLine(item.ItemId));
-
             return returnItems;
         } 
     
@@ -90,8 +85,7 @@ namespace TDD
         public string ItemId;     
         public ItemTypeEnum ItemType; 
 
-        public Item(string itemId, ItemTypeEnum itemType)     
-        {         
+        public Item(string itemId, ItemTypeEnum itemType) {         
             ItemId = itemId;
             ItemType = itemType;     
         } 
@@ -105,25 +99,27 @@ namespace TDD
  //[TestFixture]
    public class ProgramTests
     {   
+        
         [Fact]
-        public void Should_Return_RandomItems_top_two_pretest()
-        {
-            List<Item> mainItems = new List<Item>();
-            mainItems.Add( new Item("operational_item1", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("operational_item2", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("operational_item4", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("operational_item5", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("operational_item3", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("pretest_item1", ItemTypeEnum.Pretest));
-            mainItems.Add( new Item("pretest_item2", ItemTypeEnum.Pretest));
-            mainItems.Add( new Item("operational_item6", ItemTypeEnum.Operational));
-            mainItems.Add( new Item("pretest_item3", ItemTypeEnum.Pretest));
-            mainItems.Add( new Item("pretest_item4", ItemTypeEnum.Pretest));
+        public void Should_Return_RandomQuestions_Top_Two_Pretest() {
+           
+            List<Item> testQuestions = new List<Item>();
+            testQuestions.Add( new Item("operational_item1", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("operational_item2", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("operational_item4", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("operational_item5", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("operational_item3", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("pretest_item1", ItemTypeEnum.Pretest));
+            testQuestions.Add( new Item("pretest_item2", ItemTypeEnum.Pretest));
+            testQuestions.Add( new Item("operational_item6", ItemTypeEnum.Operational));
+            testQuestions.Add( new Item("pretest_item3", ItemTypeEnum.Pretest));
+            testQuestions.Add( new Item("pretest_item4", ItemTypeEnum.Pretest));
 
-            List<Item> result = Program.CreateTestAndRandomize("ID1", mainItems);
+            List<Item> resultQuestions = Program.CreateTestAndRandomize("ID1", testQuestions);
             //make sure the top 2 items in the list are pretest
-            bool bothPretest = (result.ToArray()[0].ItemType ==ItemTypeEnum.Pretest && result.ToArray()[1].ItemType ==ItemTypeEnum.Pretest)?true:false;
-            Assert.True(bothPretest);
+            bool isFirstAndSecondPretest = (resultQuestions.ToArray()[0].ItemType == ItemTypeEnum.Pretest && resultQuestions.ToArray()[1].ItemType == ItemTypeEnum.Pretest)?true:false;
+            
+            Assert.True(isFirstAndSecondPretest);
 
         }
     }
